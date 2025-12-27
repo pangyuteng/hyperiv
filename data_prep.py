1b79161206e582e3a4b727de6f3677a0f100afc8
pangyuteng <pangyuteng@gmail.com>
2025-12-27 18:27:46 +0000

    added notes

--- data_prep.py
+++ data_prep.py
@@ -62,29 +62,40 @@ def gen_data(dstamp,zero_day_only=False):
         df = df[df.expiry == dstamp]
 
     expiry_mapper = {x:get_expiry_tstamp(x) for x in df.expiry.unique()}
-    df['date']=df.tstamp_min # 
+    # TODO: drop "date" this to every 5 seconds.
+    df['date']=df.tstamp_min
     df['forward_price']=df.underlying_price
     df['tau']=df.apply(lambda x: get_annualized_time_to_expiration(x,expiry_mapper),axis=1)
     df['risk_free_rate']=1e-7
+    # TODO: train call and put seperately
     df['is_call']=df.option_type.apply(lambda x: 1.0 if x == 'call' else -1.0)
     df['strike_price']=df.strike
     df['option_price']=df.price
-    df['log_moneyness']= np.log(df.underlying_price/df.strike)
-    # df.implied_volatility
-    # df.delta
-    df['time_to_maturity']=((df.tau*TOTAL_SECONDS_ONE_YEAR)/(60*60*24)) #???
+    df['log_moneyness'] = np.log(df.underlying_price/df.strike)
+    #df.implied_volatility # same name, no need to make new column
+    #df.delta # same name, no need to make new column
+    df['time_to_maturity']=((df.tau*TOTAL_SECONDS_ONE_YEAR)/(60*60*24))
 
     df = df[(df.tau>0)&(df.log_moneyness.notnull())]
     df = df[cols]
-    df['is_ref'] = (np.random.rand(len(df)) > 0.8).astype(int)
+
+    if True:
+        raise NotImplementedError() # see note below.
+        df['is_ref'] = (np.random.rand(len(df)) > 0.8).astype(int)
 
     if False:
-        #
-        # NOTE: probably we want this? but we can't, as 
+        # NOTE: BELOW LOGIC NEEDED
+        # just not like that.
+        # we need to resample data every 5 sec
+        # grab last IV for each strike and expiry
+        # drop if sample size too small or ffill.
+        raise NotImplementedError()
+
+        # old NOTE: probably we want this? but we can't, as 
         # you don't control the expiry or delta of the contracts coming in
         # ***but*** you are using 1-min interval, which is not great, but you could maybe get fixed delta,expiry
         #
-        target_ttms = [0,1,2,3]
+        target_ttms = [0,1,5,15]
         target_deltas = [0.5, 0.25, -0.25]
         df_tmp = df.groupby('date').apply(find_closest_elements, 'time_to_maturity', target_ttms, include_groups=False)
         reference_options = df_tmp.groupby(['date','time_to_maturity']).apply(find_closest_elements, 'delta', target_deltas, include_groups=False)
