2c4db8af0a5f2d1cd13b08d7962576ff0dcafc8d
pangyuteng <pangyuteng@gmail.com>
2026-01-02 05:06:45 +0000

    wip Large dataset

--- inference.py
+++ inference.py
@@ -39,7 +39,7 @@ def load_model(model_pth_file):
 if __name__ == "__main__":
     #h5_file = "/mnt/hd2bak/scratch/spx_w_ref.h5"
     #raise ValueError()
-    model_pth_file = './workdir/spx_model_call_00002.pth'
+    model_pth_file = './workdir/spx_model_put_00009.pth'
     h5_file = "/mnt/hd2bak/scratch/spx_w_ref_put.h5"
 
     model = load_model(model_pth_file)
